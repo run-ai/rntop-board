ab798e0763a4afc4dc048e38d254720fcbb219eb
Raz Rotenberg <raz.rotenberg@gmail.com>
2022-05-11 16:12:47 +0300

    Improvements in faker.js
    - Supporting specifying # of devices
    - Supporting specifying time using days and/or hours

--- faker.js
+++ faker.js
@@ -10,8 +10,10 @@ const parser = new ArgumentParser({
 
 parser.add_argument('-o', '--output', { default: 'rntop.log', help: 'Output file' });
 parser.add_argument('--interval', { type: 'int', default: 30, help: 'Interval in seconds between cluster samples' });
-parser.add_argument('--hours', { type: 'int', default: 24 * 5, help: 'Duration of log in hours' });
+parser.add_argument('--days', { type: 'int', default: 0, help: 'Duration of log in days' });
+parser.add_argument('--hours', { type: 'int', default: 0, help: 'Duration of log in hours in addition to days' });
 parser.add_argument('--nodes', { type: 'int', default: 5, help: 'Number of nodes in the cluster' });
+parser.add_argument('--devices', { type: 'int', default: -1, help: 'Number of GPUs per node' });
 parser.add_argument('-r', '--randomness', { type: 'int', default: 10, help: 'Number of intervals to slightly change the mean node utilization' });
 
 const args = parser.parse_args();
@@ -20,7 +22,7 @@ var nodes = []
 for (let i = 0; i < args.nodes; i++) {
     nodes.push({
         name: 'server-' + i.toString(),
-        devices: _.sample([1, 2, 4, 8, 16]),
+        devices: args.devices != -1 ? args.devices : _.sample([1, 2, 4, 8, 16]),
         utilization: _.random(1, 20) * 5,
     })
 }
@@ -28,8 +30,10 @@ for (let i = 0; i < args.nodes; i++) {
 var output = fs.createWriteStream(args.output)
 output.write('timestamp,node,index,utilization,memory_used,memory_total\n')
 
+const hours = (args.days || args.hours) ? (args.days * 24 + args.hours) : 5 * 24
+
 let time = new Date();
-time.setHours(time.getHours() - args.hours)
+time.setHours(time.getHours() - hours)
 
 let counter = 0
 while (time <= new Date()) {
